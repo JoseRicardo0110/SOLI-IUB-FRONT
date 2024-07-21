f6af085899eb11b9c207ab20827bac62bd4b6c3c
JoseRicardo0110 <165341012+JoseRicardo0110@users.noreply.github.com>
2024-07-21 08:51:33 -0500

    asassa

--- frontend/src/components/formulario/formulario1..jsx
+++ frontend/src/components/formulario/formulario1..jsx
@@ -1,53 +1,91 @@
 import React from "react";
-import {} from "react-bootstrap";
 import { useNavigate } from "react-router";
 import { sesion } from "../../sesion";
+import { useState, useEffect } from 'react';
+import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
 
 export default function Formulario1() {
+  const clientID = "152246615921-plavoo3cte9rc53n7pdfn9p7lbgm2oa1.apps.googleusercontent.com";
+  const [user, setUser] = useState({});
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const onSuccess = (response) => {
+    setUser(response.profileObj);
+    setLoggedIn(true);
+    document.getElementsByClassName("btn")[0].hidden = true;
+  };
+
+  const onFailure = (response) => {
+    console.log("Something went wrong", response);
+  };
+
+  const handleLogout = () => {
+    setUser({});
+    setLoggedIn(false);
+  };
+
   const navigate = useNavigate();
   const handlelogin = () => {
     var user_usuario = document.getElementById("user_usuario").value;
     var user_contrasena = document.getElementById("user_contrasena").value;
     sesion(user_usuario, user_contrasena, navigate);
   };
-  return (
-    <div class="bgl_login pt-4 pb-4">
-      <div class="fondo conlogin shadow  ">
-        <div class="">
-          <div class="fs-1 tituloformulogin fw-bold mt-5 text-light">
-            <p>Inicio de sesion</p>
-          </div>
-          <form class="posicion_imput2 ">
-            <div class="  tamano_imput posicion_imput1 ">
-              <label class="form-label text-light">CORREO</label>
-              <input
-                class="form-control  bg-light"
-                type="text"
-                placeholder="usuario"
-                id="user_usuario"
-              />
-            </div>
-            <div class=" mt-3 tamano_imput">
-              <label class="form-label text-light">CONTRASEÑA</label>
-              <input
-                class="form-control bg-light"
-                type="password"
-                placeholder="contraseña"
-                id="user_contrasena"
-              />
-            </div>
-            <div class="mt-4 tamano_imput">
-              <button
-                class="btn btn-primary w-100  mt-4 fw-semibold shadow-sm"
-                type="button"
-                onClick={handlelogin}>
-                Iniciar sesión
-              </button>
 
+  return (
+    <GoogleOAuthProvider clientId={clientID}>
+      <div className="bgl_login pt-4 pb-4">
+        <div className="fondo conlogin shadow">
+          <div className="">
+            <div className="fs-1 tituloformulogin fw-bold mt-5 text-light">
+              <p>Inicio de sesion</p>
             </div>
-          </form>
+            <form className="posicion_imput2">
+              <div className="tamano_imput posicion_imput1">
+                <label className="form-label text-light">CORREO</label>
+                <input
+                  className="form-control bg-light"
+                  type="text"
+                  placeholder="usuario"
+                  id="user_usuario"
+                />
+              </div>
+              <div className="mt-3 tamano_imput">
+                <label className="form-label text-light">CONTRASEÑA</label>
+                <input
+                  className="form-control bg-light"
+                  type="password"
+                  placeholder="contraseña"
+                  id="user_contrasena"
+                />
+              </div>
+              <div className="mt-4 tamano_imput">
+                <button
+                  className="btn btn-primary w-100 mt-4 fw-semibold shadow-sm"
+                  type="button"
+                  onClick={handlelogin}
+                >
+                  Iniciar sesión
+                </button>
+              </div>
+              <div className='btn'>
+                <GoogleLogin
+                  onSuccess={onSuccess}
+                  onError={onFailure}
+                />
+              </div>
+              {loggedIn && (
+                <div className="profile">
+                  <img src={user.imageUrl} alt="User Profile" />
+                  <h3>{user.name}</h3>
+                  <button onClick={handleLogout} className="btn btn-secondary">
+                    Logout
+                  </button>
+                </div>
+              )}
+            </form>
+          </div>
         </div>
       </div>
-    </div>
+    </GoogleOAuthProvider>
   );
 }
