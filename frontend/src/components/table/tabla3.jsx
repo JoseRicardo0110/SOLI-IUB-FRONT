6f235f420b4ac2bb378519701c24169114d2407c
JoseRicardo0110 <165341012+JoseRicardo0110@users.noreply.github.com>
2024-08-09 18:54:34 -0500

    tt

--- frontend/src/components/table/tabla3.jsx
+++ frontend/src/components/table/tabla3.jsx
@@ -127,7 +127,7 @@ function Table3() {
           <div class="modal-dialog">
             <div class="modal-content">
               <div class="modal-header">
-                <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                <h1 class="modal-title fs-5" id="exampleModalLabel">Toma tu solicitud</h1>
                 <button type="button"  class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
               </div>
               <div class="modal-body">
